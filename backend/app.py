8c0d2689f39149db9a3dbe5f9be0b3d12f396487
= <=>
2025-10-17 11:25:32 +0530

    Ai move updated at js

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)  # uses 'templates/' and 'static/' by default
+app = Flask(__name__, template_folder="templates", static_folder="statics")
 
 @app.route('/')
-def index():
+def home():
+    return render_template('home.html')
+
+@app.route('/vs-computer')
+def vs_computer():
     return render_template('index.html')
+
+@app.route('/vs-friend')
+def vs_friend():
+    return render_template('vs-friend.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
